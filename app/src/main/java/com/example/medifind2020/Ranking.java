641fe0cb482f63dd732c9ee9bfdb67a7082cbaad
Nawamin Sukhothai <minsk783@gmail.com>
2020-10-04 00:40:56 +0700

    Add ranking layout

---
+++ app/src/main/java/com/example/medifind2020/Ranking.java
@@ -0,0 +1,14 @@
+package com.example.medifind2020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Ranking extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ranking);
+    }
+}
