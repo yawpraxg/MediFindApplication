67d62877e8a273f93dd0f7614910e6f4ebb04764
Nawamin Sukhothai <minsk783@gmail.com>
2020-12-07 23:40:01 +0700

    Update 4.1.1
    - Add UI features to show picture more than 2 (3 at maximum)
    - Fix an issues that cause the application crash

--- app/src/main/java/com/example/medifind2020/MedItem.java
+++ app/src/main/java/com/example/medifind2020/MedItem.java
@@ -7,13 +7,13 @@ import java.util.Map;
 
 public class MedItem {
     public String id;
-    public String name, gen_name, color, size, prop, dosage,side_eff, thumbnail;
+    public String name, gen_name, color, size, prop, dosage,side_eff, thumbnail, img1, img2;
 
 
 
     public MedItem() { }
 
-    public MedItem(String name, String gen_name, String color, String size, String prop, String dosage, String side_eff, String thumbnail) {
+    public MedItem(String name, String gen_name, String color, String size, String prop, String dosage, String side_eff, String thumbnail, String img1, String img2) {
         this.name = name;
         this.gen_name = gen_name;
         this.color = color;
@@ -22,6 +22,8 @@ public class MedItem {
         this.dosage = dosage;
         this.side_eff = side_eff;
         this.thumbnail = thumbnail;
+        this.img1 = img1;
+        this.img2 = img2;
     }
 
     public MedItem(Map<String, Object> map, String id) {
@@ -35,6 +37,8 @@ public class MedItem {
             this.dosage = map.get("dosage").toString();
             this.side_eff = map.get("side_eff").toString();
             this.thumbnail = map.get("thumbnail").toString();
+            this.img1 = map.get("img1").toString();
+            this.img2 = map.get("img2").toString();
 
             Log.d("MedItem", Arrays.toString(map.keySet().toArray()));
         } catch (NullPointerException e) {
@@ -74,6 +78,10 @@ public class MedItem {
         return thumbnail;
     }
 
+    public String getImg1() { return img1; }
+
+    public String getImg2() { return img2; }
+
     public void setBrandName(String name) {
         this.name = name;
     }
@@ -104,6 +112,14 @@ public class MedItem {
 
     public void setThumbnail(String image) {
         this.thumbnail = image;
+    }
+
+    public void setImg1(String img1) {
+        this.img1 = img1;
+    }
+
+    public void setImg2(String img2) {
+        this.img2 = img2;
     }
 
     public String getId() {
